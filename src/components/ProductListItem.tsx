880bf76d18236ba2d254d6bba1f90219b04bf741
Johan León <64605783+JohanLeon0914@users.noreply.github.com>
2024-07-20 09:38:16 -0500

    menu screen and flatlist

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,45 @@
+import Colors from "@/src/constants/Colors";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Product } from "../app/types";
+
+type ProductListItemProps = {
+    product: Product;
+}
+
+const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} resizeMode="contain" />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: '50%'
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
